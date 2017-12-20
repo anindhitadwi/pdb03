637b8f17d55f9918ddd4a67b77d32881ac23047a
Widiarto Adiyoso <widiartoaww@gmail.com>
2017-12-20 15:34:38 +0700

    2nd commit

---
+++ src/main/java/com/apa/Pdb03Application.java
@@ -0,0 +1,12 @@
+package com.apa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pdb03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pdb03Application.class, args);
+	}
+}
